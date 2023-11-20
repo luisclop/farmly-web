590dc35ae46985cec51902e5e33e299a44a4d64f
Luis Lopez <lclopez220@gmail.com>
2023-11-20 11:46:38 -0500

    Vite and Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="flex bg-slate-200 h-screen w-screen justify-center">
+      <div className="bg-emerald-500 h-10 w-40 flex justify-center items-center text-slate-200">
+        <p>Hello world</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
